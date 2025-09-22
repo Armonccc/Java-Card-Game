77c3b84bc98e2f726006bca1931cb4715c5b4a90
Armonccc <ascchima@gmail.com>
2025-09-21 22:48:43 -0700

    Add project files

---
+++ Check/java.java
@@ -0,0 +1,41 @@
+package Check;
+
+public class java {
+
+
+//     public String a;
+
+//     public java(String a){
+
+//         this.a = a;
+
+
+//     }
+
+//     public String memory(){
+
+//         return a;
+//     }
+//     public static void main (String args[]){
+
+//         java jav = new java("hello");
+        
+
+// System.out.println(jav.memory());
+
+//     }
+
+
+
+public static void main(String[] args) {
+    String a = "Hello";
+
+    double b= 10.0;
+    System.out.println(a);
+}
+
+
+    
+}
+
+
